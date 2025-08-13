866c924a0df01a39232ae3453f70a8c6957a06f6
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-08-13 22:08:50 +0300

    Add project files.

---
+++ Demo session 6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_session_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
